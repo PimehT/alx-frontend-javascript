36934bca886de0f97ede494fe9b76c1b558e5664
PimehT <tareerepimeh@gmail.com>
2024-06-25 00:15:45 +0100

    ES6-10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
-    // eslint-disable-next-line no-param-reassign
     array[array.indexOf(value)] = appendString + value;
   }
 
