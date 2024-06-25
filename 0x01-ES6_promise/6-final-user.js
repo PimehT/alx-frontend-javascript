b57cc699bb54df6f8f19575fcb2a5f5778e3da41
PimehT <tareerepimeh@gmail.com>
2024-06-25 21:52:12 +0100

    ES6 promise settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,12 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
-      results.forEach((result) => {
-        if (result.status === 'fulfilled') {
-          console.log(result.value);
-        } else if (result.status === 'rejected') {
-          console.log(console.log(result.reason.message));
-        }
-      });
+      results.map((result) => ({
+        status: result.status,
+        value: result.value || result.reason,
+      }));
     });
 }
