efec06568107a9b168dce7c91457426c0ad2568c
PimehT <tareerepimeh@gmail.com>
2024-06-25 21:11:43 +0100

    ES6 promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const [photo, user] = results;
-      console.log(photo.body, user.firstName, user.lastName);
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
