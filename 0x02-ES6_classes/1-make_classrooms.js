59243e94aa13492d4aac7d3bf22b8157a7231220
PimehT <tareerepimeh@gmail.com>
2024-06-27 20:29:52 +0100

    ES6 classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,5 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const rooms = [19, 20, 34];
-  const create = rooms.map((x) => new ClassRoom(x));
-
-  console.log(create);
+  return [19, 20, 34].map((x) => new ClassRoom(x));
 }
