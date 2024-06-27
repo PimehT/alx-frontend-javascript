8d3cda0371865ec72c56629c55e74b37ab0820e8
PimehT <tareerepimeh@gmail.com>
2024-06-27 22:47:15 +0100

    ES6 classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof (sqft) !== 'number') {
+      throw new Error('Must be number');
+    }
+    this._sqft = sqft;
+
+    if (
+      this.constructor !== Building
+      && this.evacuationWarningMessage === undefined
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof (sqft) !== 'number') {
+      throw new Error('Must be number');
+    }
+    this._sqft = sqft;
+  }
+}
