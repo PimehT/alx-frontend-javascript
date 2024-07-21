99bb28f8f2fe5feb4e72579945cc489fddc9e6a6
PimehT <tareerepimeh@gmail.com>
2024-07-21 21:27:16 +0100

    fixing errors

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,15 +1,15 @@
-/// <reference path="crud.d.ts" />
+/// <reference path="./crud.d.ts" />
 
 import { RowID, RowElement } from "./interface";
 import * as CRUD from './crud';
 
-const obj: RowElement = {
+const row: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
 }
 
-const newRowID: RowID = CRUD.insertRow(obj);
-const updatedRow: RowElement = { ...obj, age: 23 };
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 
